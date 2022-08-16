36626270c3a1429f1426bd2e7ba8787472a59e4a
Ferhat Güneş <guneshoca1@gmail.com>
2022-08-16 23:00:23 +0300

    v0

--- src/Gitignore.java
+++ src/Gitignore.java
@@ -2,6 +2,9 @@ public class Gitignore {
 
     public static void main(String[] args) {
         System.out.println("gitignore deneme  1 ");
+        System.out.println("gitignore deneme  1 ");
+        System.out.println("gitignore deneme  1 ");
+        System.out.println("gitignore deneme  1 ");
 
 
     }
